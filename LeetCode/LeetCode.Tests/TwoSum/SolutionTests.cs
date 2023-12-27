9bca2d4a6b11457ad8d3909287c689cef7331f06
brian <brianjoseph.egan@gmail.com>
2023-12-27 15:27:34 -0800

    First Attempt
    Add projects and provide implementation.

---
+++ LeetCode/LeetCode.Tests/TwoSum/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.TwoSum;
+
+namespace LeetCode.Tests.TwoSum
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2})]
+        [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        public void Test(int[] numbers, int target, int[] expected)
+        {
+            _solution.TwoSum(numbers, target).Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/TwoSum/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = 0; j < nums.Length; j++)
+                    if (i != j)
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            return new int[] { i, j };
+                        }
+                    }
+            }
+
+            return new int[0];
+        }
+    }
+}
