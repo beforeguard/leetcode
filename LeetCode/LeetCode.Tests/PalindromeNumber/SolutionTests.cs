a4539b525ee55840ada36581cc5b803ee8e53e58
Brian Egan <brianjoseph.egan@gmail.com>
2023-12-27 17:21:07 -0800

    Merge 9-pallindrome-number into main (#2)
    * Add Solution and SolutionTests
    
    Add implementation and test file.
    
    * Solution Implementation
    
    Implement passing solution.

---
+++ LeetCode/LeetCode.Tests/PalindromeNumber/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.PalindromeNumber;
+
+namespace LeetCode.Tests.PalindromeNumber
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        [InlineData(123, false)]
+        public void Test(int number, bool expected)
+        {
+            _solution.IsPalindrome(number).Should().Be(expected);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/PalindromeNumber/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+            if (x >= 0 && x < 10)
+            {
+                return true;
+            }
+            if (x % 10 == 0)
+            {
+                return false;
+            }
+
+            var reversed = 0;
+            var unreversed = x;
+
+            while (x != 0)
+            {
+                var digit = x % 10;
+                reversed = (reversed * 10) + digit;
+                x /= 10;
+            }
+
+            return reversed == unreversed;
+        }
+    }
+}
