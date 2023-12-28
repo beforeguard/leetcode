53eb4e3ce0601bc4c9fb2cc7fad873fb5f3eef48
brian <brianjoseph.egan@gmail.com>
2023-12-28 12:23:26 -0800

    Implementation
    Add implementation to convert Roman numeral to integer.

--- LeetCode/LeetCode/RomanToInteger/Solution.cs
+++ LeetCode/LeetCode/RomanToInteger/Solution.cs
@@ -2,9 +2,45 @@
 {
     public class Solution
     {
+        private readonly string _subtractionChars = "IXC";
+        private readonly Dictionary<string, int> _map = new Dictionary<string, int>()
+        {
+            { "I", 1 },
+            { "IV", 4 },
+            { "V", 5 },
+            { "IX", 9 },
+            { "X", 10 },
+            { "XL", 40 },
+            { "L", 50 },
+            { "XC", 90 },
+            { "C", 100 },
+            { "CD", 400 },
+            { "D", 500 },
+            { "CM", 900 },
+            { "M", 1000},
+        };
+
         public int RomanToInt(string s)
         {
-            throw new NotImplementedException();
+            var result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var c = s[i].ToString();
+
+                if (_subtractionChars.Contains(c))
+                {
+                    if (i + 1 <= s.Length - 1 && _map.ContainsKey($"{c}{s[i + 1]}"))
+                    {
+                        c += s[i + 1];
+                        i++;
+                    }
+                }
+
+                result += _map[c];
+            }
+
+            return result;
         }
     }
 }
