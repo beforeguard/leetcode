f6775b81f8e0732eacd0b5c5d912a8afb24ab596
brian <brianjoseph.egan@gmail.com>
2023-12-28 09:02:51 -0800

    Implementation and Tests
    Add solution implementation and tests.

---
+++ LeetCode/LeetCode.Tests/ValidParentheses/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.ValidParentheses;
+
+namespace LeetCode.Tests.ValidParentheses
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        [InlineData("]", false)]
+        public void Tests(string input, bool expected)
+        {
+            _solution.IsValid(input).Should().Be(expected);
+        }
+    }
+}
