10f0ece0b7c7915aa5907f7e3a9ce636949f7e11
brian <brianjoseph.egan@gmail.com>
2024-01-01 09:07:36 -0800

    Add Solution, SolutionTests and ListNode Files
    Add files.

---
+++ LeetCode.Tests/MergeTwoSortedLists/SolutionTests.cs
@@ -0,0 +1,52 @@
+﻿using LeetCode.MergeTwoSortedLists;
+
+namespace LeetCode.Tests.MergeTwoSortedLists
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 4 }, new int[] { 1, 3, 4 }, new int[] { 1, 1, 2, 3, 4, 4 })]
+        [InlineData(new int[0], new int[0], new int[0])]
+        [InlineData(new int[0], new int[] { 0 }, new int[] { 0 })]
+        public void Test(int[] numbers1, int[] numbers2, int[] expected)
+        {
+            var mergedList = _solution.MergeTwoLists(
+                ArrayToLinkedList(numbers1),
+                ArrayToLinkedList(numbers2));
+
+            LinkedListToArray(mergedList).Should().BeEquivalentTo(expected);
+        }
+
+        internal ListNode ArrayToLinkedList(int[] numbers)
+        {
+            ListNode listNode = null;
+
+            foreach (int number in numbers)
+            {
+                listNode = new ListNode(number, listNode);
+            }
+
+            return listNode;
+        }
+
+        internal int[] LinkedListToArray(ListNode listNode)
+        {
+            if (listNode == null)
+            {
+                return new int[0];
+            }
+
+            var list = new List<int>();
+
+            while (listNode != null)
+            {
+                list.Add(listNode.val);
+                listNode = listNode.next;
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ LeetCode/MergeTwoSortedLists/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.MergeTwoSortedLists
+{
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
