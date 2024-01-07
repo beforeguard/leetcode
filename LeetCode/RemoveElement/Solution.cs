5c4ee49ef12a586676d10b6e235df7a4ffda6c00
Brian Egan <brianjoseph.egan@gmail.com>
2024-01-07 08:59:46 -0800

    Solution
    Implement accepted solution.

--- LeetCode/RemoveElement/Solution.cs
+++ LeetCode/RemoveElement/Solution.cs
@@ -4,7 +4,30 @@
     {
         public int RemoveElement(int[] nums, int val)
         {
-            throw new NotImplementedException();
+            var replaceIndex = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    SwapValuesAtIndices(nums, i, replaceIndex);
+                    replaceIndex++;
+                }
+            }
+
+            return replaceIndex;
+        }
+
+        private void SwapValuesAtIndices(int[] nums, int index1, int index2)
+        {
+            if (nums[index1] == nums[index2])
+            {
+                return;
+            }
+
+            nums[index1] ^= nums[index2];
+            nums[index2] ^= nums[index1];
+            nums[index1] ^= nums[index2];
         }
     }
 }
