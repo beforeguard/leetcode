5654a4f58a7ccf3aaeb34b490a54ec1398d6f23d
Brian Egan <brianjoseph.egan@gmail.com>
2024-01-06 18:29:29 -0800

    26 remove duplicates from sorted array (#7)
    * Fix Project Strutcture
    
    Move implementation and test files for LongestCommonPrefix problem.
    
    * Skeleton Files
    
    Add Solution and SolutionTests
    
    * Add Skeleton Tests and Implenetation
    
    Add some unit tests and empty method for solution.
    
    * Implement Working Solution
    
    Add solution to problem.

---
+++ LeetCode.Tests/RemoveDuplicatesFromSortedArray/SolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.RemoveDuplicatesFromSortedArray;
+
+namespace LeetCode.Tests.RemoveDuplicatesFromSortedArray
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        [Theory]
+        [InlineData(new int[] { 1, 1, 2 }, 2)]
+        [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        public void Test(int[] nums, int expected)
+        {
+            _solution.RemoveDuplicates(nums).Should().Be(expected);
+        }
+    }
+}
---
+++ LeetCode/RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.RemoveDuplicatesFromSortedArray
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var hashSet = new HashSet<int>();
+            var replaceIndex = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (!hashSet.Contains(nums[i]))
+                {
+                    hashSet.Add(nums[i]);
+                    SwapValuesAtIndices(nums, i, replaceIndex);
+                    replaceIndex++;
+                }
+            }
+
+            return hashSet.Count;
+        }
+
+        private void SwapValuesAtIndices(int[] nums, int index1, int index2)
+        {
+            if (nums[index1] == nums[index2])
+            {
+                return;
+            }
+
+            nums[index1] ^= nums[index2];
+            nums[index2] ^= nums[index1];
+            nums[index1] ^= nums[index2];
+        }
+    }
+}
