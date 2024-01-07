4e4126d966a9d33f85574bafcf6afadfd73f4d26
Brian Egan <brianjoseph.egan@gmail.com>
2024-01-06 18:29:03 -0800

    Implement Working Solution
    Add solution to problem.

--- LeetCode/RemoveDuplicatesFromSortedArray/Solution.cs
+++ LeetCode/RemoveDuplicatesFromSortedArray/Solution.cs
@@ -4,7 +4,32 @@
     {
         public int RemoveDuplicates(int[] nums)
         {
-            throw new NotImplementedException();
+            var hashSet = new HashSet<int>();
+            var replaceIndex = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (!hashSet.Contains(nums[i]))
+                {
+                    hashSet.Add(nums[i]);
+                    SwapValuesAtIndices(nums, i, replaceIndex);
+                    replaceIndex++;
+                }
+            }
+
+            return hashSet.Count;
+        }
+
+        private void SwapValuesAtIndices(int[] nums, int index1, int index2)
+        {
+            if (nums[index1] == nums[index2])
+            {
+                return;
+            }
+
+            nums[index1] ^= nums[index2];
+            nums[index2] ^= nums[index1];
+            nums[index1] ^= nums[index2];
         }
     }
 }
