daa04bad8a66d45e0bd1ec764ded85ae1e4d424e
Brian Egan <brianjoseph.egan@gmail.com>
2024-01-13 13:04:15 -0800

    Add Solution and SolutionTests
    Add files with skeleton implementation.

---
+++ LeetCode/PlusOne/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.PlusOne
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
