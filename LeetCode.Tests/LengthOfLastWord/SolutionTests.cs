d6842f3795743b530a14235f0a07869577392bc2
Brian Egan <brianjoseph.egan@gmail.com>
2024-01-13 09:54:37 -0800

    Add Solution and SolutionTests.
    Add Solution and SolutionTests.

---
+++ LeetCode.Tests/LengthOfLastWord/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.LengthOfLastWord;
+
+namespace LeetCode.Tests.LengthOfLastWord
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new Solution();
+
+        [Theory]
+        [InlineData("Hello World", 5)]
+        [InlineData("   fly me   to   the moon  ", 4)]
+        [InlineData("luffy is still joyboy", 6)]
+        public void Test(string s, int expected)
+        {
+            _solution.LengthOfLastWord(s).Should().Be(expected);
+        }
+    }
+}
