daba62f7419d595d8032fb781f4b4af4771d8246
Brian Egan <brianjoseph.egan@gmail.com>
2024-03-31 19:34:24 -0700

    Refactor ListNode Class
    Refactor to remove duplicate class and add extension methods to help with test classes.

--- LeetCode.Tests/RemoveDuplicatesFromSortedList/SolutionTests.cs
+++ LeetCode.Tests/RemoveDuplicatesFromSortedList/SolutionTests.cs
@@ -11,39 +11,9 @@ namespace LeetCode.Tests.RemoveDuplicatesFromSortedList
         [InlineData(new int[] { 1, 1, 2, 3, 3 }, new int[] { 1, 2, 3 })]
         public void Test(int[] nums, int[] expected)
         {
-            var result = _solution.DeleteDuplicates(ArrayToLinkedList(nums));
+            var result = _solution.DeleteDuplicates(nums.ToLinkedList());
 
-            LinkedListToArray(result).Should().BeEquivalentTo(expected);
-        }
-
-        private ListNode ArrayToLinkedList(int[] numbers)
-        {
-            ListNode listNode = null;
-
-            for (var i = numbers.Length - 1; i >= 0; i--)
-            {
-                listNode = new ListNode(numbers[i], listNode);
-            }
-
-            return listNode;
-        }
-
-        private int[] LinkedListToArray(ListNode listNode)
-        {
-            if (listNode == null)
-            {
-                return [];
-            }
-
-            var list = new List<int>();
-
-            while (listNode != null)
-            {
-                list.Add(listNode.val);
-                listNode = listNode.next;
-            }
-
-            return list.ToArray();
+            result.ToArray().Should().BeEquivalentTo(expected);
         }
     }
 }
