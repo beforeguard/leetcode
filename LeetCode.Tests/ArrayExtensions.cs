e7b3d8febc5b09d8e76ab55d69f63afd089a7854
Brian Egan <brianjoseph.egan@gmail.com>
2024-03-31 19:34:57 -0700

    83 remove duplicates from sorted list (#13)
    * Remove Duplicates From Sorted List
    
    Solution and Solution Tests.
    
    * Refactor ListNode Class
    
    Refactor to remove duplicate class and add extension methods to help with test classes.

---
+++ LeetCode.Tests/ArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Tests
+{
+    internal static class ArrayExtensions
+    {
+        internal static ListNode ToLinkedList(this int[] numbers)
+        {
+            ListNode listNode = null;
+
+            for (var i = numbers.Length - 1; i >= 0; i--)
+            {
+                listNode = new ListNode(numbers[i], listNode);
+            }
+
+            return listNode;
+        }
+    }
+}
