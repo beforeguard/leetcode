49531590e97eef5a39fcd43015fcbf865eb8bc08
Brian Egan <brianjoseph.egan@gmail.com>
2024-04-28 08:49:08 -0700

    94 binary tree inorder traversal (#15)
    * Add implementation and test code. Missing solution to convert array to binary tree in tests.
    
    * Add Implementation for ToBinaryTree
    
    Add implementation.

--- LeetCode.Tests/ArrayExtensions.cs
+++ LeetCode.Tests/ArrayExtensions.cs
@@ -1,17 +1,57 @@
-﻿namespace LeetCode.Tests
+﻿using System.Linq;
+
+namespace LeetCode.Tests
 {
     internal static class ArrayExtensions
     {
         internal static ListNode ToLinkedList(this int[] numbers)
         {
-            ListNode listNode = null;
+            ListNode head = null;
 
             for (var i = numbers.Length - 1; i >= 0; i--)
             {
-                listNode = new ListNode(numbers[i], listNode);
+                head = new ListNode(numbers[i], head);
             }
 
-            return listNode;
+            return head;
+        }
+
+        internal static TreeNode ToBinaryTree(this int?[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                return null;
+            }
+
+            var treeNodeQueue = new Queue<TreeNode>();
+            var integerQueue = new Queue<int?>();
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                integerQueue.Enqueue(numbers[i]);
+            }
+
+            var treeNode = new TreeNode(numbers[0].Value);
+            treeNodeQueue.Enqueue(treeNode);
+
+            while (integerQueue.Any())
+            {
+                var leftValue = !integerQueue.Any() ? null : integerQueue.Dequeue();
+                var rightValue = !integerQueue.Any() ? null : integerQueue.Dequeue();
+                var current = treeNodeQueue.Dequeue();
+                if (leftValue.HasValue)
+                {
+                    var left = new TreeNode(leftValue.Value);
+                    current.left = left;
+                    treeNodeQueue.Enqueue(left);
+                }
+                if (rightValue.HasValue)
+                {
+                    var right = new TreeNode(rightValue.Value);
+                    current.right = right;
+                    treeNodeQueue.Enqueue(right);
+                }
+            }
+            return treeNode;
         }
     }
 }
---
+++ LeetCode/BinaryTreeInorderTraversal/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.BinaryTreeInorderTraversal
+{
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            InorderTraversal(result, root);
+            return result;
+        }
+
+        private void InorderTraversal(IList<int> nums, TreeNode current)
+        {
+            if (current == null)
+            {
+                return;
+            }
+
+            InorderTraversal(nums, current.left);
+            nums.Add(current.val);
+            InorderTraversal(nums, current.right);
+        }
+    }
+}
