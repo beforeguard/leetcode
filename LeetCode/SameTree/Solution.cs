437d490e5bb01e690aed4ad8930f80ec549f01a6
Brian Egan <brianjoseph.egan@gmail.com>
2024-08-11 12:32:15 -0700

    Add Implemenation
    Add code to solve problem.

--- LeetCode/SameTree/Solution.cs
+++ LeetCode/SameTree/Solution.cs
@@ -4,7 +4,19 @@
     {
         public bool IsSameTree(TreeNode p, TreeNode q)
         {
-            throw new NotImplementedException();
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p != null && q != null)
+            {
+                return p.val == q.val &&
+                    IsSameTree(p.left, q.left) &&
+                    IsSameTree(p.right, q.right);
+            }
+
+            return false;
         }
     }
 }
