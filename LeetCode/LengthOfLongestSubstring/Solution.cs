0ba5db76a2d53ad51b012e24a6eb0c68dc8d11cb
Brian Egan <brianjoseph.egan@gmail.com>
2024-12-02 10:59:50 -0800

    Solution and SolutionTests.

---
+++ LeetCode/LengthOfLongestSubstring/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.LengthOfLongestSubstring
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var hashSet = new HashSet<char>();
+
+            var result = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                hashSet.Clear();
+                var substringLength = 0;
+                var j = i;
+                while (j < s.Length && !hashSet.Contains(s[j]))
+                {
+                    hashSet.Add(s[j++]);
+                    substringLength++;
+                }
+
+                if (substringLength > result)
+                {
+                    result = substringLength;
+                }
+            }
+
+            return result;
+        }
+    }
+}
