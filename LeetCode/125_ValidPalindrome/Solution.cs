bec59f21baae85cff3abcc64eb3265e5ade1cd59
Brian Egan <brianjoseph.egan@gmail.com>
2025-02-13 16:59:17 -0800

    125 Valid Palindrome
    Add Solution and SolutionTests.

---
+++ LeetCode/125_ValidPalindrome/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace LeetCode._125_ValidPalindrome
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in s)
+            {
+                if (char.IsDigit(c) || char.IsLetter(c))
+                {
+                    stringBuilder.Append(char.ToLower(c));
+                }
+            }
+
+            return stringBuilder
+                .ToString()
+                .Equals(new string(stringBuilder.ToString().Reverse().ToArray()));
+        }
+    }
+}
