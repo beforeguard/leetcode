3f242122d190409dbc96a7e8b2b35286e428e986
Brian Egan <brianjoseph.egan@gmail.com>
2025-02-15 11:43:05 -0800

    Work in progress.

--- LeetCode.Tests/69_SquareRoot/SolutionTests.cs
+++ LeetCode.Tests/69_SquareRoot/SolutionTests.cs
@@ -1,14 +1,19 @@
-﻿using LeetCode.SquareRoot;
+﻿using LeetCode._69_SquareRoot;
 
-namespace LeetCode.Tests.SquareRoot
+namespace LeetCode.Tests._69_SquareRoot
 {
     public class SolutionTests
     {
         private readonly Solution _solution = new();
 
         [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
         [InlineData(4, 2)]
+        [InlineData(6, 2)]
         [InlineData(8, 2)]
+        [InlineData(36, 6)]
         public void Test(int x, int expected)
         {
             _solution.MySqrt(x).Should().Be(expected);
---
+++ LeetCode/69_SquareRoot/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode._69_SquareRoot
+{
+    public class Solution
+    {
+        public int MySqrt(int x)
+        {
+            return MySqrt(x, 0, x / 2);
+        }
+
+        private int MySqrt(int x, int low, int high)
+        {
+            if (high >= low)
+            {
+                var mid = low + (high - 2) / 2;
+                var midSqred = mid * mid;
+
+                if (midSqred == x)
+                {
+                    return mid;
+                }
+                else if (midSqred > x)
+                {
+                    return MySqrt(x, low, mid - 1);
+                }
+                else
+                {
+                    return MySqrt(x, mid + 1, high);
+                }
+            }
+            else
+            {
+                return low;
+            }
+        }
+    }
+}
