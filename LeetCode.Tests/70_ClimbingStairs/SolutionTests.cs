df13ac5ccce436d52c0eb6f7a3f44d474cacb8f2
Brian Egan <brianjoseph.egan@gmail.com>
2025-02-20 18:59:31 -0800

    70 Climbing Stairs (#23)
    Add Solution and SolutionTests.

---
+++ LeetCode.Tests/70_ClimbingStairs/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode._70_ClimbingStairs;
+
+namespace LeetCode.Tests._70_ClimbingStairs
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(45, 1836311903)]
+        public void Test(int n, int expected)
+        {
+            _solution.ClimbStairs(n).Should().Be(expected);
+        }
+    }
+}
---
+++ LeetCode/70_ClimbingStairs/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode._70_ClimbingStairs
+{
+    public class Solution
+    {
+        private readonly Dictionary<int, int> _memoization = new();
+
+        public int ClimbStairs(int n) => Fib(n);
+
+        private int Fib(int n)
+        {
+            if (n <= 2)
+            {
+                return n;
+            }
+            else if (_memoization.TryGetValue(n, out var storedVal))
+            {
+                return storedVal;
+            }
+            else
+            {
+                var val = Fib(n - 1) + Fib(n - 2);
+                _memoization.Add(n, val);
+                return val;
+            }
+        }
+    }
+}
