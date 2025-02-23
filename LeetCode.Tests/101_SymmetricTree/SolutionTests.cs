ce92477ef69cf686f0997628262e752f7534da6f
Brian Egan <brianjoseph.egan@gmail.com>
2025-02-22 18:34:57 -0800

    101 Symmetric Tree
    Add Solution and SolutionTests.

---
+++ LeetCode.Tests/101_SymmetricTree/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode._101_SymmetricTree;
+
+namespace LeetCode.Tests._101_SymmetricTree
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        public static TheoryData<int?[], bool> TestData => new TheoryData<int?[], bool>
+        {
+            { new int?[] { 1,2,2,3,4,4,3 }, true },
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int?[] testData, bool expected)
+        {
+            var result = _solution.IsSymmetric(testData.ToBinaryTree());
+
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ LeetCode/101_SymmetricTree/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._101_SymmetricTree
+{
+    public class Solution
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null || root.left == null && root.right == null)
+            {
+                return true;
+            }
+            else
+            {
+                return AreNodesEqual(root.left, root.right);
+            }
+        }
+
+        private bool AreNodesEqual(TreeNode leftNode, TreeNode rightNode)
+        {
+            return (leftNode == null && rightNode == null) ||
+                (leftNode?.val == rightNode?.val) &&
+                AreNodesEqual(leftNode.left, rightNode.right) &&
+                AreNodesEqual(leftNode.right, rightNode.left);
+        }
+    }
+}
