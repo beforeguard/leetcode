030f82102c07ca76e5c6839ccc882dcb2d85e9de
Brian Egan <brianjoseph.egan@gmail.com>
2025-02-24 18:25:43 -0800

    104 Maximum Depth of Binary Tree
    Add Solution and SolutionTests.

---
+++ LeetCode.Tests/104_MaximumDepthOfBinaryTree/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode._104_MaximumDepthOfBinaryTree;
+
+namespace LeetCode.Tests._104_MaximumDepthOfBinaryTree
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        public static TheoryData<int?[], int> TestData => new TheoryData<int?[], int>
+        {
+            { new int?[] { 3, 9, 20, null, null, 15, 7 }, 3 },
+            { new int?[] { 1, null, 2 }, 2 },
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int?[] testData, int expected)
+        {
+            var result = _solution.MaxDepth(testData.ToBinaryTree());
+
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ LeetCode/104_MaximumDepthOfBinaryTree/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._104_MaximumDepthOfBinaryTree
+{
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                var leftHeight = MaxDepth(root.left);
+                var rightHeight = MaxDepth(root.right);
+
+                return Math.Max(leftHeight, rightHeight) + 1;
+            }
+        }
+    }
+}
