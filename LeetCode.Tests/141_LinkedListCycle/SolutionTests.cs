6a79381f56465538c9083136ca4d192c05183e0f
Brian Egan <brianjoseph.egan@gmail.com>
2025-03-08 17:08:31 -0800

    141 Linked List Cycle
    Add Solution and SolutionTests.

---
+++ LeetCode.Tests/141_LinkedListCycle/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode._141_LinkedListCycle;
+
+namespace LeetCode.Tests._141_LinkedListCycle
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        [Theory]
+        [InlineData(new int[] { 3, 2, 0, -4 }, 1, true)]
+        [InlineData(new int[] { 1, 2 }, 0, true)]
+        [InlineData(new int[] { 1 }, -1, false)]
+        public void Test(int[] nums, int cycleIndex, bool expected)
+        {
+            _solution.HasCycle(nums.ToLinkedList(cycleIndex)).Should().Be(expected);
+        }
+    }
+}
