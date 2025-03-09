62d8141719920cab9bed908c1d8692827482c51e
Brian Egan <brianjoseph.egan@gmail.com>
2025-03-08 18:17:08 -0800

    136 Single Number (#27)
    Add Solution and SolutionTests.

---
+++ LeetCode.Tests/136_SingleNumber/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode._136_SingleNumber;
+
+namespace LeetCode.Tests._136_SingleNumber
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        [Theory]
+        [InlineData(new int[] { 2, 2, 1 }, 1)]
+        [InlineData(new int[] { 4, 1, 2, 1, 2 }, 4)]
+        [InlineData(new int[] { 1 }, 1)]
+        public void Test(int[] nums, int expected)
+        {
+            _solution.SingleNumber(nums).Should().Be(expected);
+        }
+    }
+}
---
+++ LeetCode/136_SingleNumber/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._136_SingleNumber
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var xor = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                xor ^= nums[i];
+            }
+
+            return xor;
+        }
+    }
+}
