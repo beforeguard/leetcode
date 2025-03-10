3b24471933ebd2206d110838f6f1730cba32e003
Brian Egan <brianjoseph.egan@gmail.com>
2025-03-09 17:59:21 -0700

    Add Solution and SolutionTests

---
+++ LeetCode/145_BinaryTreePostorderTraversal/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._145_BinaryTreePostorderTraversal
+{
+    public class Solution
+    {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            PostorderTraversal(root, result);
+            return result;
+        }
+
+        private void PostorderTraversal(TreeNode node, List<int> nums)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            PostorderTraversal(node.left, nums);
+            PostorderTraversal(node.right, nums);
+            nums.Add(node.val);
+
+        }
+    }
+}
