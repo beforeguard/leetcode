d77e730869724d58b3df9a563a1e170600f7f06a
Brian Egan <brianjoseph.egan@gmail.com>
2025-03-09 18:01:35 -0700

    145 Binary Tree Postorder Traversal (#31)

---
+++ LeetCode.Tests/145_BinaryTreePostorderTraversal/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode._145_BinaryTreePostorderTraversal;
+
+namespace LeetCode.Tests._145_BinaryTreePostorderTraversal
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        public static TheoryData<int?[], int[]> TestData => new TheoryData<int?[], int[]>
+        {
+            { new int?[] { 1, null ,2 , 3 }, new int[] { 3, 2, 1 } },
+            { new int?[] { 1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9 }, new int[] { 4, 6, 7, 5, 2, 9, 8, 3, 1 } },
+            { new int?[] {}, new int[] {} },
+            { new int?[] { 1 }, new int[] { 1 } },
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Fact(int?[] nums, int[] expected)
+        {
+            _solution.PostorderTraversal(nums.ToBinaryTree()).Should().BeEquivalentTo(expected);
+        }
+    }
+}
