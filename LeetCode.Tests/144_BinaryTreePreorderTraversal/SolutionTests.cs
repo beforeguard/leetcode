d7f6779754b749ce1fd0c793a1940212919cdc53
Brian Egan <brianjoseph.egan@gmail.com>
2025-03-09 17:45:22 -0700

    Add Solution and SolutionTests.

---
+++ LeetCode.Tests/144_BinaryTreePreorderTraversal/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode._144_BinaryTreePreorderTraversal;
+
+namespace LeetCode.Tests._144_BinaryTreePreorderTraversal
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        public static TheoryData<int?[], int[]> TestData => new TheoryData<int?[], int[]>
+        {
+            { new int?[] { 1, null, 2, 3 }, new int[] { 1, 2, 3 } },
+            { new int?[] { 1,2,3,4,5,null,8,null,null,6,7,9 }, new int[] { 1, 2, 4, 5, 6, 7, 3, 8, 9 } },
+            { new int?[] {}, new int[] {} },
+            { new int?[] { 1 }, new int[] { 1 } },
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Fact(int?[] nums, int[] expected)
+        {
+            _solution.PreorderTraversal(nums.ToBinaryTree()).Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ LeetCode/144_BinaryTreePreorderTraversal/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode._144_BinaryTreePreorderTraversal
+{
+    public class Solution
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            PreorderTraversal(root, result);
+            return result;
+        }
+
+        private void PreorderTraversal(TreeNode node, List<int> nums)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            nums.Add(node.val);
+            PreorderTraversal(node.left, nums);
+            PreorderTraversal(node.right, nums);
+        }
+    }
+}
