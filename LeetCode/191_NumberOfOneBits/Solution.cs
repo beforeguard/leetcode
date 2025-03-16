27658e52ebb51e2f2dafd9acb67486cd55205d22
Brian Egan <brianjoseph.egan@gmail.com>
2025-03-16 16:05:42 -0700

    191 Number of 1 Bits (#35)

---
+++ LeetCode/191_NumberOfOneBits/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace LeetCode._191_NumberOfOneBits
+{
+    public class Solution
+    {
+        public int HammingWeight(int n)
+        {
+            var bitArray = new BitArray(new int[] { n });
+
+            var result = 0;
+            for (var i = 0; i < bitArray.Length; i++)
+            {
+                result += bitArray[i] ? 1 : 0;
+            }
+            return result;
+        }
+    }
+}
