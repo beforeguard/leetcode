c831f6d4f2801459165c55fa128c0f49f7c44db4
Brian Egan <brianjoseph.egan@gmail.com>
2025-03-16 15:17:33 -0700

    Add Solution and SolutionTests.

---
+++ LeetCode.Tests/169_MajorityElement/SolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode._169_MajorityElement;
+
+namespace LeetCode.Tests._169_MajorityElement
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new();
+
+        [Theory]
+        [InlineData(new int[] { 3, 2, 3 }, 3)]
+        [InlineData(new int[] { 2, 2, 1, 1, 1, 2, 2 }, 2)]
+        public void Test(int[] nums, int expected)
+        {
+            _solution.MajorityElement(nums).Should().Be(expected);
+        }
+    }
+}
