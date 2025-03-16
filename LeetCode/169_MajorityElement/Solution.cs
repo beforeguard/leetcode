13f0095d5a6de9343507517ee5b1ae1602cdcc55
Brian Egan <brianjoseph.egan@gmail.com>
2025-03-16 15:18:35 -0700

    Add Solution and SolutionTests. (#33)

---
+++ LeetCode/169_MajorityElement/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode._169_MajorityElement
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Array.Sort(nums);
+            return nums[nums.Length / 2];
+        }
+    }
+}
