35cdb2add4697c08e0a0133bea0e1c2157e435f3
Brian Egan <brianjoseph.egan@gmail.com>
2025-03-16 16:05:14 -0700

    191 Number of 1 Bits

---
+++ LeetCode.Tests/191_NumberOfOneBits/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode._191_NumberOfOneBits;
+
+namespace LeetCode.Tests._191_NumberOfOneBits
+{
+    public class SolutionTests
+    {
+        private Solution _solution = new();
+
+        [Theory]
+        [InlineData(11, 3)]
+        [InlineData(128, 1)]
+        [InlineData(2147483645, 30)]
+        public void Test(int num, int expected)
+        {
+            _solution.HammingWeight(num).Should().Be(expected);
+        }
+    }
+}
