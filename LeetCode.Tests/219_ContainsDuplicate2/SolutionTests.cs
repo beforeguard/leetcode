7dfab8e0fd0f7bd00c03cab469dbfff8b9e7001e
Brian Egan <brianjoseph.egan@gmail.com>
2025-04-06 10:50:47 -0700

    Add Solution and SolutionTests.

---
+++ LeetCode.Tests/219_ContainsDuplicate2/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode._219_ContainsDuplicate2;
+
+namespace LeetCode.Tests._219_ContainsDuplicate2
+{
+    public class SolutionTests
+    {
+        private readonly Solution _solution = new Solution();
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 1 }, 3, true)]
+        [InlineData(new[] { 1, 0, 1, 1 }, 1, true)]
+        [InlineData(new[] { 1, 2, 3, 1, 2, 3 }, 2, false)]
+        public void ContainsNearbyDuplicate(int[] nums, int k, bool expected)
+        {
+            _solution.ContainsNearbyDuplicate(nums, k).Should().Be(expected);
+        }
+    }
+}
---
+++ LeetCode/219_ContainsDuplicate2/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode._219_ContainsDuplicate2
+{
+    public class Solution
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (dict.TryGetValue(nums[i], out var lastIndex))
+                {
+                    if (Math.Abs(i - lastIndex) <= k)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        dict[nums[i]] = i;
+                    }
+                }
+                else
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+
+            return false;
+        }
+    }
+}
